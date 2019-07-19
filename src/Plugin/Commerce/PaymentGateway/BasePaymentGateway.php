ad0772e1a306f3972f0a54a3703eec6414231507
Christian Wiedemann <christian.wiedemann@key-tec.de>
2019-07-19 21:56:03 +0200

    Add stored payment methods.

--- src/Plugin/Commerce/PaymentGateway/BasePaymentGateway.php
+++ src/Plugin/Commerce/PaymentGateway/BasePaymentGateway.php
@@ -3,9 +3,13 @@
 namespace Drupal\commerce_crefopay\Plugin\Commerce\PaymentGateway;
 
 use Drupal\commerce_crefopay\Client\Builder\IdBuilder;
+use Drupal\commerce_crefopay\Client\OrderIdAlreadyExistsException;
+use Drupal\commerce_crefopay\Client\SubscriptionClient;
 use Drupal\commerce_crefopay\Client\TransactionClient;
+use Drupal\commerce_crefopay\ConfigProviderInterface;
 use Drupal\commerce_order\Entity\OrderInterface;
 use Drupal\commerce_payment\Entity\PaymentInterface;
+use Drupal\commerce_payment\Exception\PaymentGatewayException;
 use Drupal\commerce_payment\PaymentMethodTypeManager;
 use Drupal\commerce_payment\PaymentTypeManager;
 use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayBase;
@@ -13,6 +17,7 @@ use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\SupportsRefundsInterf
 use Drupal\commerce_price\Price;
 use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\user\Entity\User;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -28,12 +33,26 @@ class BasePaymentGateway extends OffsitePaymentGatewayBase implements SupportsRe
   protected $transactionClient;
 
   /**
+   * The transaction client.
+   *
+   * @var \Drupal\commerce_crefopay\Client\TransactionClient
+   */
+  protected $subscriptionClient;
+
+  /**
    * The IdBuilder.
    *
    * @var \Drupal\commerce_crefopay\Client\Builder\IdBuilder
    */
   protected $idBuilder;
 
+  /**
+   * The config provider.
+   *
+   * @var \Drupal\commerce_crefopay\ConfigProviderInterface
+   */
+
+  protected $configProvider;
   /**
    * The logger.
    *
@@ -60,17 +79,106 @@ class BasePaymentGateway extends OffsitePaymentGatewayBase implements SupportsRe
    *   The time.
    * @param \Drupal\commerce_crefopay\Client\TransactionClient $transaction_client
    *   The transaction client.
+   * @param \Drupal\commerce_crefopay\Client\SubscriptionClient $subscription_client
+   *   The subscription client.
+   * @param \Drupal\commerce_crefopay\ConfigProviderInterface $config_provider
+   *   The subscription client.
    * @param \Drupal\commerce_crefopay\Client\Builder\IdBuilder $id_builder
    *   The id builder.
    * @param Psr\Log\LoggerInterface
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, PaymentTypeManager $payment_type_manager, PaymentMethodTypeManager $payment_method_type_manager, TimeInterface $time, TransactionClient $transaction_client, IdBuilder $id_builder, LoggerInterface $logger) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, PaymentTypeManager $payment_type_manager, PaymentMethodTypeManager $payment_method_type_manager, TimeInterface $time, ConfigProviderInterface $config_provider, TransactionClient $transaction_client, SubscriptionClient $subscription_client, IdBuilder $id_builder, LoggerInterface $logger) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $payment_type_manager, $payment_method_type_manager, $time);
     $this->transactionClient = $transaction_client;
+    $this->subscriptionClient = $subscription_client;
+    $this->configProvider = $config_provider;
     $this->idBuilder = $id_builder;
     $this->logger = $logger;
   }
 
+  /**
+   * Calls a crefopay create transaction or create subscription based the order type.
+   *
+   * @param \Drupal\commerce_payment\Entity\PaymentInterface $payment
+   *   The current payment.
+   * @return \Upg\Library\Request\Objects\PaymentInstrument[]
+   *   Payment instruments.
+   */
+  public function createTransaction (PaymentInterface $payment) {
+    $order = $payment->getOrder();
+    $billing_profile = $order->getBillingProfile();
+    /** @var \Drupal\address\Plugin\Field\FieldType\AddressItem $address_item */
+    $address = $billing_profile->address[0];
+    $user = User::load(\Drupal::currentUser()->id());
+
+    $instruments = NULL;
+    $subscription_order_type_id = $this->configProvider->getSubscriptionOrderTypeId();
+    if ($subscription_order_type_id === $order->bundle()) {
+      $items = $order->getItems();
+      $plan_reference = NULL;
+      foreach ($items as $item) {
+        $purchased_product = $item->getPurchasedEntity();
+        if ($purchased_product->hasField('crefopay_subscription_plan') &&
+          $purchased_product->crefopay_subscription_plan->value != NULL) {
+          $plan_reference = $purchased_product->crefopay_subscription_plan->value;
+          break;
+        }
+      }
+      if ($plan_reference == NULL) {
+        throw new PaymentGatewayException('Unknown subscription plan. Please check product configuration.');
+      }
+      try {
+        $instruments = $this->subscriptionClient->createSubscription($order, $user, $address, $plan_reference);
+      }
+      catch (OrderIdAlreadyExistsException $oe) {
+        //throw new PaymentGatewayException('Order already exists.');
+        // Transaction already started.
+      }
+      catch (\Throwable $exception) {
+        throw new PaymentGatewayException($exception);
+      }
+    }
+    else {
+      /** @var \Drupal\commerce_crefopay\Client\TransactionClient $transaction_client */
+      try {
+        $instruments = $this->transactionClient->createTransaction($order, $user, $address, "SecureFields");
+        /** @var \Drupal\commerce_crefopay\Client\Builder\IdBuilder $id_builder */
+      }
+      catch (OrderIdAlreadyExistsException $oe) {
+        //throw new PaymentGatewayException('Order already exists.');
+        // Transaction already started.
+      }
+      catch (\Throwable $exception) {
+        throw new PaymentGatewayException($exception);
+      }
+
+    }
+
+    if ($instruments == NULL) {
+      $instruments = $this->transactionClient->getTransactionPaymentInstruments($order);
+    }
+    return $instruments;
+  }
+
+  /**
+   * Returns the config provider.
+   *
+   * @return \Drupal\commerce_crefopay\ConfigProviderInterface
+   *   The config provider.
+   */
+  public function getConfigProvider(){
+    return $this->configProvider;
+  }
+
+  /**
+   * Returns the idBuilder.
+   *
+   * @return \Drupal\commerce_crefopay\Client\Builder\IdBuilder
+   *   The idBuilder
+   */
+  public function getIdBuilder(){
+    return $this->idBuilder;
+  }
   /**
    * {@inheritdoc}
    */
@@ -83,7 +191,9 @@ class BasePaymentGateway extends OffsitePaymentGatewayBase implements SupportsRe
       $container->get('plugin.manager.commerce_payment_type'),
       $container->get('plugin.manager.commerce_payment_method_type'),
       $container->get('datetime.time'),
+      $container->get('commerce_crefopay.config_provider'),
       $container->get('commerce_crefopay.transaction_client'),
+      $container->get('commerce_crefopay.subscription_client'),
       $container->get('commerce_crefopay.id_builder'),
       $container->get('logger.channel.commerce_payment')
     );
@@ -116,7 +226,7 @@ class BasePaymentGateway extends OffsitePaymentGatewayBase implements SupportsRe
     $payment_storage = $this->entityTypeManager->getStorage('commerce_payment');
     $remote_state = $transaction_status['transactionStatus'];
     switch ($remote_state) {
-      case "Done":
+      case "DONE":
         {
           $state = "completed";
           break;
@@ -126,22 +236,23 @@ class BasePaymentGateway extends OffsitePaymentGatewayBase implements SupportsRe
           $state = "new";
           break;
         }
-      case "AcknowledgePending":
-      case "FraudPending":
-      case "MerchantPending":
-      case "CIAPending":
+      case "ACKNOWLEDGEPENDING":
+      case "FRAUDPENDING":
+      case "MERCHANTPENDING":
+      case "CIAPENDING":
+      case "INPROGRESS":
         {
           $state = "authorization";
           break;
         }
 
-      case "Cancelled":
-      case "FraudCancelled":
+      case "CANCELLED":
+      case "FRAUDCANCELLED":
         {
           $state = "refunded";
           break;
         }
-      case "Expired":
+      case "EXPIRED":
         {
           $state = 'authorization_expired';
           break;
@@ -158,7 +269,7 @@ class BasePaymentGateway extends OffsitePaymentGatewayBase implements SupportsRe
       'amount' => $order->getBalance(),
       'payment_gateway' => $this->entityId,
       'order_id' => $order->id(),
-      'remote_id' => $transaction_status['additionalData']['paymentReference'],
+      'remote_id' => $order->id(),
       'remote_state' => $remote_state,
     ]);
     $payment->save();
--- src/PluginForm/SecureFields/SecureFieldsForm.php
+++ src/PluginForm/SecureFields/SecureFieldsForm.php
@@ -2,11 +2,8 @@
 
 namespace Drupal\commerce_crefopay\PluginForm\SecureFields;
 
-use Drupal\commerce_crefopay\Client\OrderIdAlreadyExistsException;
-use Drupal\commerce_payment\Exception\PaymentGatewayException;
 use Drupal\commerce_payment\PluginForm\PaymentOffsiteForm as BasePaymentOffsiteForm;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\user\Entity\User;
 
 class SecureFieldsForm extends BasePaymentOffsiteForm {
 
@@ -18,82 +15,40 @@ class SecureFieldsForm extends BasePaymentOffsiteForm {
 
     /** @var \Drupal\commerce_payment\Entity\PaymentInterface $payment */
     $payment = $this->entity;
-    /** @var \Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayInterface $payment_gateway_plugin */
+    /** @var \Drupal\commerce_crefopay\Plugin\Commerce\PaymentGateway\SecureFields $payment_gateway_plugin */
+    $payment_gateway_plugin = $payment->getPaymentGateway()->getPlugin();
+
     $order = $payment->getOrder();
-    $billing_profile = $order->getBillingProfile();
-    /** @var \Drupal\address\Plugin\Field\FieldType\AddressItem $address_item */
-    $address = $billing_profile->address[0];
+    $instruments = $payment_gateway_plugin->createTransaction($payment);
 
-    $user = User::load(\Drupal::currentUser()->id());
-    /** @var \Drupal\commerce_crefopay\ConfigProviderInterface $config_provider */
-    $config_provider = \Drupal::service('commerce_crefopay.config_provider');
-    $subscription_order_type_id = $config_provider->getSubscriptionOrderTypeId();
-    $transaction_client = \Drupal::service('commerce_crefopay.transaction_client');
+    $config_provider = $payment_gateway_plugin->getConfigProvider();
 
-    if ($subscription_order_type_id === $order->bundle()) {
-      $items = $order->getItems();
-      $plan_reference = NULL;
-      foreach ($items as $item) {
-        $purchased_product = $item->getPurchasedEntity();
-        if ($purchased_product->hasField('crefopay_subscription_plan') &&
-          $purchased_product->crefopay_subscription_plan->value != NULL) {
-          $plan_reference = $purchased_product->crefopay_subscription_plan->value;
-          break;
-        }
-      }
-      if ($plan_reference == NULL) {
-        throw new PaymentGatewayException('Unknown subscription plan. Please check product configuration.');
-      }
-      /** @var \Drupal\commerce_crefopay\Client\SubscriptionClient $subscription_client */
-      $subscription_client = \Drupal::service('commerce_crefopay.subscription_client');
-      try {
-        $subscription_client->createSubscription($order, $user, $address, $plan_reference);
-      }
-      catch (OrderIdAlreadyExistsException $oe) {
-        //throw new PaymentGatewayException('Order already exists.');
-        // Transaction already started.
-      }
-      catch (\Throwable $exception) {
-        throw new PaymentGatewayException('Unexcpected error.');
-      }
-    }
-    else {
-      /** @var \Drupal\commerce_crefopay\Client\TransactionClient $transaction_client */
-      try {
-        $transaction_client->createTransaction($order, $user, $address, "SecureFields");
-        /** @var \Drupal\commerce_crefopay\Client\Builder\IdBuilder $id_builder */
-      }
-      catch (OrderIdAlreadyExistsException $oe) {
-        //throw new PaymentGatewayException('Order already exists.');
-        // Transaction already started.
-      }
-      catch (\Throwable $exception) {
-        throw new PaymentGatewayException('Unexcpected error.');
-      }
-
-    }
-
-    $instruments = $transaction_client->getTransactionPaymentInstruments($order);
-    $id_builder = \Drupal::service('commerce_crefopay.id_builder');
-    /** @var \Drupal\commerce_crefopay\ConfigProvider $config_provider */
-    $config_provider = \Drupal::service('commerce_crefopay.config_provider');
     $config_array = $config_provider->getConfigArray();
     $secure_fields_url = $config_provider->getSecureFieldsUrl('test');
+
+    $allowed_intruments_twig = [];
+    $allowed_intruments = $instruments['allowedPaymentInstruments'];
+
+    /** @var \Upg\Library\Request\Objects\PaymentInstrument $allowed_intrument */
+    foreach ($allowed_intruments as $allowed_intrument) {
+      $allowed_intruments_twig[] = $allowed_intrument->toArray();
+    }
 
     $form['crefopay_payment'] = [
       '#theme' => 'crefopay_payment',
       '#allowed_methods' => array_fill_keys($instruments['allowedPaymentMethods'], true),
+      '#allowed_intruments' => $allowed_intruments_twig,
       '#additional_information' => $instruments['additionalInformation'],
       '#attached' => [
         'library' => ['commerce_crefopay/crefopay'],
         'drupalSettings' => [
         'crefopay' => [
-          'orderId' => $id_builder->id($order),
+          'orderId' => $payment_gateway_plugin->getIdBuilder()->id($order),
           'placeholder' => [],
           'secureFieldsUrl' => $secure_fields_url,
-          'shopPublicKey' => $config_array['shopPublicKey']
-          ]
-        ]
+          'shopPublicKey' => $config_array['shopPublicKey'],
+          ],
+        ],
       ],
     ];
 
